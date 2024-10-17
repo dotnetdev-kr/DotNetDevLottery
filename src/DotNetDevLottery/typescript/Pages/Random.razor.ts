8497a0c4bf48669e27c07ac46b175e69d6d422ef
Yunsup Sim <pedogunu@gmail.com>
2024-10-17 19:55:40 +0900

    feat: Disposal 구현 (#8)

--- src/DotNetDevLottery/typescript/Pages/Random.razor.ts
+++ src/DotNetDevLottery/typescript/Pages/Random.razor.ts
@@ -3,7 +3,9 @@ import '@lottiefiles/dotlottie-wc';
 
 function startLottie(element: DotLottieWC) {
   element.dotLottie?.setFrame(0);
+  element.dotLottie?.unfreeze();
   element.dotLottie?.play();
+  console.log(element.dotLottie?.isPlaying);
 }
 
 export { startLottie }
