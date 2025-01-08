33d8e341e7da1647dc1246b18fc8d4826572f15e
SimYunSup <dev@ethansup.net>
2025-01-08 10:16:37 +0000

    Deploying to gh-pages from @ dotnetdev-kr/DotNetDevLottery@d7566d33b7d57038e866e99a8229a9970dce41a5 🚀

--- js/Components/MachineAnimation.razor.js
+++ js/Components/MachineAnimation.razor.js
@@ -183,7 +183,7 @@ async function defineDrawMachineAnimation({
     size
   });
   const topCollider = sphere.find((part) => part.angle === Math.PI * 3 / 2).collider;
-  const renderedBallCount = count > 20 ? count : 20;
+  const renderedBallCount = count;
   let balls = addBallsToEngine({
     engine,
     rapier,
@@ -335,9 +335,9 @@ var Ball = class _Ball {
     this.#element.classList.add("ball");
     this.#size = size;
     this.#ballSize = ballSize;
-    this.#element.style.setProperty(_Ball.BALL_SIZE_PROPERTY_NAME, String(this.#ballSize * 2));
+    this.#element.style.setProperty(_Ball.BALL_SIZE_PROPERTY_NAME, String(this.#ballSize));
     wrapperElement.appendChild(this.#element);
-    const rapierBallSize = coords.toRapier.length(ballSize);
+    const rapierBallSize = coords.toRapier.length(ballSize / 2);
     const bodyDesc = new rapier.RigidBodyDesc(rapier.RigidBodyType.Dynamic).setTranslation(
       ...coords.toRapier.vector(size.wrapper / 2 + index, size.wrapper / 2 + index)
     ).setCcdEnabled(true);
@@ -350,7 +350,7 @@ var Ball = class _Ball {
       return false;
     }
     const [x, y] = coords.fromBody.vector(this.#body);
-    this.#element.style.transform = `translate(${x - this.#ballSize}px, ${y - this.#ballSize}px) scale(1)`;
+    this.#element.style.transform = `translate(${x - this.#ballSize / 2}px, ${y - this.#ballSize / 2}px) scale(1)`;
     return true;
   }
   destroy() {
@@ -397,7 +397,7 @@ function addBallsToEngine(option) {
     element
   } = option;
   const balls = Array.from({ length: count }, (_, index) => {
-    const BALL_SIZE = size.machine / (count * 0.75);
+    const BALL_SIZE = size.machine / (Math.sqrt(count) * 1.5 + 2);
     const ball = new Ball({
       engine,
       rapier,
