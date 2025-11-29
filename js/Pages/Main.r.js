8cd3d1331fc87961485245bfe639bd4b50acf3fa
SimYunSup <dev@ethansup.net>
2025-11-29 11:32:05 +0000

    Deploying to gh-pages from @ dotnetdev-kr/DotNetDevLottery@18c01672d010817baf8e9fa4e4324c9f434d0294 🚀

--- js/Pages/Main.r.js
+++ js/Pages/Main.r.js
@@ -3,13 +3,26 @@ function clickElement(element) {
   element?.click();
 }
 function addDropEventToChangeInputFile(element) {
-  element.addEventListener("sp-dropzone-drop", (e) => {
-    const event = e;
+  element.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  });
+  element.addEventListener("dragenter", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+  });
+  element.addEventListener("drop", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
     const targetInputElement = element.querySelector("input[type='file']");
-    if (event.detail.dataTransfer?.files && targetInputElement) {
-      targetInputElement.files = event.detail.dataTransfer.files;
-      const changeEvent = new Event("change");
-      targetInputElement?.dispatchEvent(changeEvent);
+    if (e.dataTransfer?.files && targetInputElement) {
+      const dt = new DataTransfer();
+      Array.from(e.dataTransfer.files).forEach((file) => {
+        dt.items.add(file);
+      });
+      targetInputElement.files = dt.files;
+      const changeEvent = new Event("change", { bubbles: true });
+      targetInputElement.dispatchEvent(changeEvent);
     }
   });
 }
